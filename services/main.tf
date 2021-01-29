bfdffb14150640efc35e15d9e09237906c06145b
Vijay Gupta <vkgupta@adobe.com>
2021-01-29 15:33:11 +0530

    Initial commit of modules repo

---
+++ services/main.tf
@@ -0,0 +1,148 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CREATE ALL THE RESOURCES TO DEPLOY AN APP IN AN AUTO SCALING GROUP WITH AN ELB
+# This template runs a simple "Hello, World" web server in Auto Scaling Group (ASG) with an Elastic Load Balancer
+# (ELB) in front of it to distribute traffic across the EC2 Instances in the ASG.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR AWS CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "/home/vk/terraform-vk/intro-to-Terraform/stage/modules/services/webserver-cluster"
+  cluster_name = "webservers-stage"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# GET THE LIST OF AVAILABILITY ZONES IN THE CURRENT REGION
+# Every AWS accout has slightly different availability zones in each region. For example, one account might have
+# us-east-1a, us-east-1b, and us-east-1c, while another will have us-east-1a, us-east-1b, and us-east-1d. This resource
+# queries AWS to fetch the list for the current account and region.
+# ---------------------------------------------------------------------------------------------------------------------
+
+data "aws_availability_zones" "all" {}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE AUTO SCALING GROUP
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+
+  min_size = var.min_size
+  max_size = var.max_size
+
+  load_balancers    = [aws_elb.example.name]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "${var.cluster_name}-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A LAUNCH CONFIGURATION THAT DEFINES EACH EC2 INSTANCE IN THE ASG
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_launch_configuration" "example" {
+  # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  # Whenever using a launch configuration with an auto scaling group, you must set create_before_destroy = true.
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE SECURITY GROUP THAT'S APPLIED TO EACH EC2 INSTANCE IN THE ASG
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "${var.cluster_name}-instance"
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE AN ELB TO ROUTE TRAFFIC ACROSS THE AUTO SCALING GROUP
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_elb" "example" {
+  name               = "${var.cluster_name}-asg-example"
+  security_groups    = [aws_security_group.elb.id]
+  availability_zones = data.aws_availability_zones.all.names
+
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  # This adds a listener for incoming HTTP requests.
+  listener {
+    lb_port           = var.elb_port
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE A SECURITY GROUP THAT CONTROLS WHAT TRAFFIC AN GO IN AND OUT OF THE ELB
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "elb" {
+  name = "${var.cluster_name}-elb"
+
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = var.elb_port
+    to_port     = var.elb_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
